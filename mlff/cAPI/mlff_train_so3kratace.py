82d80b780cdf3a7549c8527fee2da067a6e8b56b
Kevin Hoellring <kevin.hoellring@fau.de>
2026-05-14 13:54:51 +0200

    Fix distributed jax not being compatible with windows

--- mlff/cAPI/mlff_train_so3kratace.py
+++ mlff/cAPI/mlff_train_so3kratace.py
@@ -6,7 +6,7 @@ import os
 import argparse
 import wandb
 import json
-import portpicker
+import sys
 
 from pathlib import Path
 from typing import Dict
@@ -24,8 +24,10 @@ from mlff.properties import md17_property_keys
 import mlff.properties.property_names as pn
 
 # logging.basicConfig(level=logging.INFO)
-port = portpicker.pick_unused_port()
-jax.distributed.initialize(f'localhost:{port}', num_processes=1, process_id=0)
+if sys.platform != "win32":
+    import portpicker
+    port = portpicker.pick_unused_port()
+    jax.distributed.initialize(f'localhost:{port}', num_processes=1, process_id=0)
 
 
 def unit_convert_data(x: Dict, table: Dict):
--- mlff/cAPI/mlff_train_so3krates.py
+++ mlff/cAPI/mlff_train_so3krates.py
@@ -3,10 +3,10 @@ import jax
 import jax.numpy as jnp
 import logging
 import os
+import sys
 import argparse
 import wandb
 import json
-import portpicker
 
 from pathlib import Path
 from typing import Dict
@@ -25,8 +25,10 @@ from mlff.properties import md17_property_keys
 import mlff.properties.property_names as pn
 
 # logging.basicConfig(level=logging.INFO)
-port = portpicker.pick_unused_port()
-jax.distributed.initialize(f'localhost:{port}', num_processes=1, process_id=0)
+if sys.platform != "win32":
+    import portpicker
+    port = portpicker.pick_unused_port()
+    jax.distributed.initialize(f'localhost:{port}', num_processes=1, process_id=0)
 
 
 def unit_convert_data(x: Dict, table: Dict):
