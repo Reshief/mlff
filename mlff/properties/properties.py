f9e71a0f1ebc551c14b53aa0ba4191f34bbfbb95
Kevin Höllring <kevin.hoellring@fau.de>
2026-06-25 14:44:04 +0200

    Add shnitsel-data keys

--- mlff/properties/properties.py
+++ mlff/properties/properties.py
@@ -1,26 +1,41 @@
 from .property_names import *
 
-md17_property_keys = {energy: 'E',
-                      force: 'F',
-                      atomic_position: 'R',
-                      atomic_type: 'z',
-                      idx_i: 'idx_i',
-                      idx_j: 'idx_j',
-                      unit_cell: 'unit_cell',
-                      pbc: 'pbc',
-                      cell_offset: 'cell_offset',
-                      stress: 'stress',
-                      node_mask: 'node_mask'}
+md17_property_keys = {
+    energy: "E",
+    force: "F",
+    atomic_position: "R",
+    atomic_type: "z",
+    idx_i: "idx_i",
+    idx_j: "idx_j",
+    unit_cell: "unit_cell",
+    pbc: "pbc",
+    cell_offset: "cell_offset",
+    stress: "stress",
+    node_mask: "node_mask",
+}
 
-qm7x_property_keys = {energy: 'ePBE0+MBD',
-                      force: 'totFOR',
-                      atomic_position: 'atXYZ',
-                      atomic_type: 'atNUM',
-                      hirshfeld_volume: 'hVOL',
-                      hirshfeld_volume_ratio: 'hRAT',
-                      partial_charge: 'hCHG',
-                      total_dipole_moment: 'vDIP',
-                      idx_i: 'idx_i',
-                      idx_j: 'idx_j',
-                      node_mask: 'node_mask'
-                      }
+qm7x_property_keys = {
+    energy: "ePBE0+MBD",
+    force: "totFOR",
+    atomic_position: "atXYZ",
+    atomic_type: "atNUM",
+    hirshfeld_volume: "hVOL",
+    hirshfeld_volume_ratio: "hRAT",
+    partial_charge: "hCHG",
+    total_dipole_moment: "vDIP",
+    idx_i: "idx_i",
+    idx_j: "idx_j",
+    node_mask: "node_mask",
+}
+
+shnitsel_data_property_keys = {
+    energy: "energy",
+    force: "forces",
+    atomic_position: "atXYZ",
+    atomic_type: "atNums",
+    total_dipole_moment: "dip_perm",
+    idx_i: "idx_i",
+    idx_j: "idx_j",
+    node_mask: "node_mask",
+    pair_mask: "pair_mask",
+}
--- mlff/properties/property_names.py
+++ mlff/properties/property_names.py
@@ -1,24 +1,25 @@
-energy = 'energy'
-force = 'force'
-hirshfeld_volume = 'hirshfeld_volume'
-hirshfeld_volume_ratio = 'hirshfeld_volume_ratio'
-partial_charge = 'partial_charge'
-total_dipole_moment = 'total_dipole_moment'
-total_quadrupole_moment = 'total_quadrupole_moment'
-stress = 'stress'
-atomic_energy = 'atomic_energy'
+energy = "energy"
+force = "force"
+hirshfeld_volume = "hirshfeld_volume"
+hirshfeld_volume_ratio = "hirshfeld_volume_ratio"
+partial_charge = "partial_charge"
+total_dipole_moment = "total_dipole_moment"
+total_quadrupole_moment = "total_quadrupole_moment"
+stress = "stress"
+atomic_energy = "atomic_energy"
 
-atomic_position = 'atomic_position'
-atomic_type = 'atomic_type'
+atomic_position = "atomic_position"
+atomic_type = "atomic_type"
 
-total_charge = 'total_charge'
-total_spin = 'total_spin'
+total_charge = "total_charge"
+total_spin = "total_spin"
 
-idx_i = 'idx_i'
-idx_j = 'idx_j'
+idx_i = "idx_i"
+idx_j = "idx_j"
 
-node_mask = 'node_mask'
+node_mask = "node_mask"
+pair_mask = "pair_mask"
 
-unit_cell = 'unit_cell'
-cell_offset = 'cell_offset'
-pbc = 'pbc'
+unit_cell = "unit_cell"
+cell_offset = "cell_offset"
+pbc = "pbc"
